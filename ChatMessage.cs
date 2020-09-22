8a5ce45eb3cbbe12fd2df57b3d58f8c7f161c942
Deepali Suryawanshi <70803023+DeepaliSuryawanshi@users.noreply.github.com>
2020-09-22 12:36:27 +0530

    Add files via upload

---
+++ ChatMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace PushNotification1.Models
+{
+	public class ChatMessage
+	{
+		public string username { get; set; }
+		public string text { get; set; }
+		public string dt { get; set; }
+	}
+}
